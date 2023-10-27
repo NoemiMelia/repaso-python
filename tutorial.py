6f666d712567dc17d363eabc583561e862024121
Noemi <al.noemimelia@iesramonllull.net>
2023-10-27 12:56:09 +0200

    first commit

---
+++ tutorial.py
@@ -0,0 +1,21 @@
+print("Hola Mundo")
+#Variables
+texto= "Hola amigos"
+nombre= "Noemi Melià"
+altura= "153cm"
+year= "2006"
+
+print(f"{texto} - {nombre} - {altura} - {str(year)}")
+print(texto + " - " + nombre + " - " + altura + " - " + str(year))
+
+#Entrada
+#sitioweb = input("¿Cuál es tu página web?: ")
+#print("El sitio web del usuario es: " + sitioweb)
+
+# Condiciones
+altura = input("¿Cual es tu altura?: ")
+if altura >=150
+    print("Eres medianamente alto!!")
+else: 
+    print("Eres un minion!!")
+
